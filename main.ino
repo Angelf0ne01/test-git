73c4e6e8f5b41acd7943b1304e986449d30b1bc6
Angel Vazquez <angel.vazquez.dev@gmail.com>
2022-08-30 16:03:19 -0300

    agregue la feature del led

--- main.ino
+++ main.ino
@@ -1,3 +1,20 @@
+class Led{
+    int pin;
+    public:
+    Led(int pin){
+        this->pin = pin;
+        pinMode(this->pin,OUTPUT);
+    }
+
+    void on(){
+        digitalWrite(this->pin,HIGH);
+    }
+
+    void off(){
+        digitalWrite(this->pin,LOW);
+    }
+};
+
 void setup(){
 
 }
