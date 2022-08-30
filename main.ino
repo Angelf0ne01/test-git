432d131c088cd14459a845cf827e2dcc4bfc9991
Angel Vazquez <angel.vazquez.dev@gmail.com>
2022-08-30 16:07:06 -0300

    agregue la feature del buzzer

--- main.ino
+++ main.ino
@@ -1,3 +1,21 @@
+class Buzzer{
+    int pin;
+    public:
+    Buzzer(int pin){
+        this->pin = pin;
+        pinMode(this->pin,OUTPUT);
+    }
+
+    void on(){
+        digitalWrite(this->pin,HIGH);
+    }
+
+    void off(){
+        digitalWrite(this->pin,LOW);
+    }
+};
+
+
 void setup(){
 
 }
